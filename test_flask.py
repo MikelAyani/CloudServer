a316fc3ee95c6a3a96a2eef12e4fc119de50fb64
Mikel Ayani <mikel.ayani@simumatik.com>
2024-06-08 19:21:01 +0200

    datastore added

--- test_flask.py
+++ test_flask.py
@@ -1,15 +1,11 @@
 
 import requests
-url = 'https://maiklof-http-server-chi.vercel.app/api/test'
+url = 'http://localhost:5000/api/test'
+#url = 'https://maiklof-http-server-chi.vercel.app/api/test'
 json_data = {
     'Hello': 'World',
 }
 
-response = requests.post(url, json=json_data)
-if response.status_code == 200:
-    print('JSON data sent successfully!')
-else:
-    print('Failed to send JSON data:', response.status_code)
 
 response = requests.get(url)
 if response.status_code == 200:
