3728e6e481dd4ece1103e32b228ed79d0755d1ad
Mikel Ayani <mikel.ayani@simumatik.com>
2024-06-08 15:07:08 +0200

    debug added

--- index.js
+++ index.js
@@ -6,14 +6,20 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/api', (req, res) => {
+  console.log('GET /api called');
   res.send('Hello, GET request!');
 });
 
 app.post('/api', (req, res) => {
+  console.log('POST /api called');
   const data = req.body;
   res.send(`Hello, POST request! You sent: ${JSON.stringify(data)}`);
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Page not found');
 });
