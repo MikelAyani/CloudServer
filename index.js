c85bcf415fc9c4c559f6918a880dfc175365ddf2
Mikel Ayani <mikel.ayani@simumatik.com>
2024-06-08 14:25:02 +0200

    Hello World

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  });
+
 let weatherData = {};
 
 app.post('/weather', (req, res) => {
