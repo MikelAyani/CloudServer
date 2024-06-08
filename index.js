86e1c5d85d4fa03080624c7dc732ba5be6667291
Mikel Ayani <mikel.ayani@simumatik.com>
2024-06-08 12:21:48 +0200

    change to Vercel

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+let weatherData = {};
+
+app.post('/weather', (req, res) => {
+    weatherData = req.body;
+    res.send({ status: 'success' });
+});
+
+app.get('/get_weather', (req, res) => {
+    res.send(weatherData);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
