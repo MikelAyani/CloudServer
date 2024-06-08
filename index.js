1122983b9d525084335943980c27187fc9255797
Mikel Ayani <mikel.ayani@simumatik.com>
2024-06-08 15:46:28 +0200

    Dynamic content

--- index.js
+++ index.js
@@ -9,26 +9,40 @@ app.use(cors());
 // Middleware to parse JSON
 app.use(express.json());
 
+// In-memory data store
+let dataStore = {};
+
 // Log all incoming requests
 app.use((req, res, next) => {
   console.log(`Incoming request: ${req.method} ${req.originalUrl}`);
   next();
 });
 
-app.get('/api', (req, res) => {
-    console.log('GET /api called');
-    res.send('Hello, GET request!');
-  });
-
-app.get('/api/test', (req, res) => {
-console.log('GET /api/test called');
-res.send('Hello World!');
+// Flexible GET route to retrieve data
+app.get('/api/:key', (req, res) => {
+  const key = req.params.key;
+  console.log(`GET /api/${key} called`);
+  const data = dataStore[key];
+  if (data) {
+    res.json({ key, data });
+  } else {
+    res.status(404).send(`No data found for key: ${key}`);
+  }
 });
-    
-app.post('/api', (req, res) => {
-  console.log('POST /api called');
-  const data = req.body;
-  res.send(`Hello, POST request! You sent: ${JSON.stringify(data)}`);
+
+// Flexible POST route to store data
+app.post('/api/:key', (req, res) => {
+  const key = req.params.key;
+  const value = req.body;
+  console.log(`POST /api/${key} called with data: ${JSON.stringify(value)}`);
+  dataStore[key] = value;
+  res.json({ message: `Data stored for key: ${key}`, data: value });
+});
+
+// List all stored data
+app.get('/api', (req, res) => {
+  console.log('GET /api called to list all data');
+  res.json(dataStore);
 });
 
 app.listen(port, () => {
