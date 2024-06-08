01251dd65bc13a6b5525bc2f61c84689cf89a9c7
Mikel Ayani <mikel.ayani@simumatik.com>
2024-06-08 15:24:57 +0200

    more stuff

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON
 app.use(express.json());
 
+// Log all incoming requests
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.originalUrl}`);
+  next();
+});
+
 app.get('/api', (req, res) => {
   console.log('GET /api called');
   res.send('Hello, GET request!');
@@ -20,6 +26,8 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+// Default 404 handler
 app.use((req, res, next) => {
+  console.log(`404 - Not Found: ${req.originalUrl}`);
   res.status(404).send('Page not found');
 });
