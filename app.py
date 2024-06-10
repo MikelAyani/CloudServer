f1c51ff3b379b822ab4eeb64a4f3a1014e5a5ebd
Mikel Ayani <mikel.ayani@simumatik.com>
2024-06-10 21:16:21 +0200

    cleanup pandas

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from vercel_kv_sdk import KV
-import pandas as pd
-from io import StringIO
+#import pandas as pd
+#from io import StringIO
 import json
 
 app = Flask(__name__)
